9222b41a3c947f5278917f1a4ff8c2d97bd13fc4
George Gastaldi <gegastaldi@gmail.com>
2016-09-15 11:43:48 -0300

    Cleaning up

--- src/main/java/org/swarm/generator/rest/ProjectGeneratorResource.java
+++ src/main/java/org/swarm/generator/rest/ProjectGeneratorResource.java
@@ -1,11 +1,12 @@
 package org.swarm.generator.rest;
 
 import java.io.ByteArrayOutputStream;
-import java.util.Arrays;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.validation.constraints.NotNull;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -18,67 +19,56 @@ import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @ApplicationScoped
 @Path("/generator")
-public class ProjectGeneratorResource
-{
-   private static final String WILDFLY_SWARM_VERSION = "2016.8.1";
+public class ProjectGeneratorResource {
+    private static final String WILDFLY_SWARM_VERSION = "2016.9";
 
-   TemplateEngine engine;
+    TemplateEngine engine;
 
-   public ProjectGeneratorResource()
-   {
-      engine = new TemplateEngine();
-      engine.setTemplateResolver(new ClassLoaderTemplateResolver(getClass().getClassLoader()));
-   }
+    public ProjectGeneratorResource() {
+        engine = new TemplateEngine();
+        engine.setTemplateResolver(new ClassLoaderTemplateResolver(getClass().getClassLoader()));
+    }
 
-   @GET
-   @Produces("application/zip")
-   public Response generate(
-            @QueryParam("g") String groupId,
-            @QueryParam("a") String artifactId,
-            @QueryParam("dep") String[] dependencies)
-            throws Exception
-   {
-      Context context = new Context();
-      context.setVariable("groupId", groupId);
-      context.setVariable("artifactId", artifactId);
-      context.setVariable("dependencies", dependencies);
-      context.setVariable("swarmVersion", WILDFLY_SWARM_VERSION);
-      ByteArrayOutputStream baos = new ByteArrayOutputStream();
-      try (ZipOutputStream zos = new ZipOutputStream(baos))
-      {
-         zos.putNextEntry(new ZipEntry(artifactId + "/src/main/java/"));
-         zos.closeEntry();
-
-         zos.putNextEntry(new ZipEntry(artifactId + "/pom.xml"));
-         zos.write(engine.process("templates/pom.tl.xml", context).getBytes());
-         zos.closeEntry();
-
-         if (enableJAXRS(dependencies))
-         {
-            zos.putNextEntry(new ZipEntry(artifactId + "/src/main/java/com/example/rest/RestApplication.java"));
-            zos.write(engine.process("templates/RestApplication.tl.java", context).getBytes());
+    @GET
+    @Produces("application/zip")
+    public Response generate(
+            @QueryParam("g") @NotNull String groupId,
+            @QueryParam("a") @NotNull String artifactId,
+            @QueryParam("dep") List<String> dependencies)
+            throws Exception {
+        Context context = new Context();
+        context.setVariable("groupId", groupId);
+        context.setVariable("artifactId", artifactId);
+        context.setVariable("dependencies", dependencies);
+        context.setVariable("swarmVersion", WILDFLY_SWARM_VERSION);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try (ZipOutputStream zos = new ZipOutputStream(baos)) {
+            zos.putNextEntry(new ZipEntry(artifactId + "/src/main/java/"));
             zos.closeEntry();
 
-            zos.putNextEntry(new ZipEntry(artifactId + "/src/main/java/com/example/rest/HelloWorldEndpoint.java"));
-            zos.write(engine.process("templates/HelloWorldEndpoint.tl.java", context).getBytes());
+            zos.putNextEntry(new ZipEntry(artifactId + "/pom.xml"));
+            zos.write(engine.process("templates/pom.tl.xml", context).getBytes());
             zos.closeEntry();
-         }
-      }
 
-      return Response
-               .ok(baos.toByteArray())
-               .type("application/zip")
-               .header("Content-Disposition", "attachment; filename=\"" + artifactId + ".zip\"")
-               .build();
-   }
+            if (enableJAXRS(dependencies)) {
+                zos.putNextEntry(new ZipEntry(artifactId + "/src/main/java/com/example/rest/HelloWorldEndpoint.java"));
+                zos.write(engine.process("templates/HelloWorldEndpoint.tl.java", context).getBytes());
+                zos.closeEntry();
+            }
+        }
 
-   private boolean enableJAXRS(String[] dependencies)
-   {
-      if (dependencies == null || dependencies.length == 0)
-      {
-         return true;
-      }
-      return Arrays.stream(dependencies).anyMatch(d -> d.contains("jaxrs") || d.contains("microprofile"));
-   }
+        return Response
+                .ok(baos.toByteArray())
+                .type("application/zip")
+                .header("Content-Disposition", "attachment; filename=\"" + artifactId + ".zip\"")
+                .build();
+    }
+
+    private boolean enableJAXRS(List<String> dependencies) {
+        if (dependencies == null || dependencies.size() == 0) {
+            return true;
+        }
+        return dependencies.stream().anyMatch(d -> d.contains("jaxrs") || d.contains("microprofile"));
+    }
 
 }
