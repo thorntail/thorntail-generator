4fe5bc8c66b1df7c4806399856988073ff090014
Bob McWhirter <bob@mcwhirter.org>
2017-02-13 12:53:08 -0500

    SWARM-1018 - Add @ApplicationScoped to JAXRS resource if CDI. (#3)

--- src/main/resources/templates/HelloWorldEndpoint.tl.java
+++ src/main/resources/templates/HelloWorldEndpoint.tl.java
@@ -1,10 +1,12 @@
 package [# th:utext="${endpointPackage}"/];
 
+[# th:if="${dependencies.contains('cdi')||dependencies.contains('microprofile')}"]import javax.enterprise.context.ApplicationScoped;[/]
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
 
+[# th:if="${dependencies.contains('cdi')||dependencies.contains('microprofile')}"]@ApplicationScoped[/]
 @Path("/hello")
 public class HelloWorldEndpoint {
 
